fc3f3b480d13315f77f5b6536d761bbd6e8a2cb1
Gilles <gilles.audemard@gmail.com>
2026-05-10 23:37:38 +0200

    slide in progress

---
+++ src/xcsp_xml/constraint_slide.rs
@@ -0,0 +1,64 @@
+/*=============================================================================
+* RUST parser for CSP instances represented in XCSP3 Format
+*
+* Copyright (c) 2026 xcsp.org (contact @ xcsp.org)
+*
+* Based on the original Rust parser proposed in https://github.com/luhanzhen/xcsp3-rust
+* by Luhan Zhen (zhenlh20@mails.jlu.edu.cn)
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*=============================================================================
+*/
+
+pub mod xcsp3_xml {
+    use crate::xcsp_xml::constraint::xcsp3_xml::ListWithOffset;
+    use crate::xcsp_xml::constraint_type::xcsp3_xml::ConstraintType;
+    use serde::Deserialize;
+
+    /**
+    syntax.
+    ```xml
+    <group  [ id="identifier" ]>
+      <constraint.../>  <!-- constraint template -->
+      (<args> (intExpr wspace)+ </args>)2+
+    </group>
+    ```
+
+    eg.
+    ```xml
+    <group id="g">
+      <intension> eq(add(%0,%1),%2) </intension>
+      <args> x0 x1 x2 </args>
+      <args> x3 x4 x5 </args>
+      <args> x6 x7 x8 </args>
+    </group>
+    ```
+     */
+    #[derive(Deserialize, Debug)]
+    pub struct ConstraintSlide {
+        // #[serde(rename = "@id", default)]
+        // id: String,
+        #[serde(rename = "@circular", default)]
+        pub circular: String,
+        #[serde(rename = "list", default)]
+        pub arg: Box<ListWithOffset>,
+        #[serde(rename = "$value", default)]
+        pub constraint: Box<ConstraintType>,
+    }
+}
